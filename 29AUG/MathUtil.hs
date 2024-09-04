a7059c2d517cd1a4bf395b4ca45ca9cb55739500
Obskyur <parkourman.miller@gmail.com>
2024-09-04 11:09:28 -0700

    feat: switch, Map

--- 29AUG/MathUtil.hs
+++ 29AUG/MathUtil.hs
@@ -2,6 +2,7 @@ module MathUtil where
 
 -- Arithmetic:
 add x y = x + y
+inc1 = add 1
 
 sub x y = x - y
 
@@ -18,3 +19,21 @@ len2 xs = if xs == [] then 0 else 1 + len2 (tail xs)
 len3 xs
   | xs == [] = 0
   | otherwise = 1 + len3 (tail xs)
+
+len4 xs = case xs of
+  [] -> 0
+  (_:ys) -> 1 + len4 ys
+
+{- Map -}
+incrementList xs = map inc1 xs
+-- shorthand:
+-- incrementList = map inc1
+squareList xs = map (\x -> x * x) xs
+-- shorthand:
+-- squareList = map (\x -> x * x)
+
+
+{- Filter -}
+
+
+{- Fold -}
