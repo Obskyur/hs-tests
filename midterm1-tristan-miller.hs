fd0a23aa1b3d5fd1b27086cb0d69ad984c1d7b1d
Obskyur <parkourman.miller@gmail.com>
2024-10-03 17:32:38 -0700

    docs: Style comments the way I want.

--- midterm1-tristan-miller.hs
+++ midterm1-tristan-miller.hs
@@ -4,8 +4,7 @@
 {- 1. sumEvens -}
 sumEvens :: [Int] -> Int
 sumEvens [] = 0 -- Base Case
-sumEvens (x : xs) =
-  -- Recursive Case
+sumEvens (x : xs) = -- Recursive Case
   if x `mod` 2 == 0
     then x + sumEvens xs
     else sumEvens xs
@@ -19,8 +18,7 @@ sumEvens'' x = foldr (+) 0 [x | x <- x, x `mod` 2 == 0] -- List Comprehension
 {- 2. findValue -}
 findValue :: (Eq a) => a -> [(a, b)] -> Maybe b -- a must be an instance of Eq for comparison
 findValue _ [] = Nothing -- Base Case
-findValue a ((x, y) : xs) =
-  -- Recursive Case
+findValue a ((x, y) : xs) = -- Recursive Case
   if a == x -- check if key x matches a
     then Just y -- return value y
     else findValue a xs -- else try again with the rest of the list
@@ -28,8 +26,7 @@ findValue a ((x, y) : xs) =
 {- 3. mapMaybe -}
 mapMaybe :: (a -> Maybe b) -> [a] -> [b]
 mapMaybe _ [] = [] -- Base Case
-mapMaybe f (x : xs) =
-  -- Recursive Case
+mapMaybe f (x : xs) = -- Recursive Case:
   case f x of -- check if f x returns Just y or Nothing
     Just y -> y : mapMaybe f xs -- if f x returns Just y, add y to the front of the list
     Nothing -> mapMaybe f xs -- else continue with the rest of the list
